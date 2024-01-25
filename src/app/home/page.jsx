ef15dc6743959a4678a341aed1736949cc48d468
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2024-01-24 19:38:29 -0800

    testing home page redirect

---
+++ src/app/home/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+
+export default function HomePage() {
+
+
+    return (
+        <div className="p-12 flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-100 to-indigo-500">
+            <div className="p-8 flex flex-col bg-white text-gray-500 text-2xl border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 shadow-2xl">
+                <h1>Home</h1>
+            </div>
+        </div>
+    )
+}

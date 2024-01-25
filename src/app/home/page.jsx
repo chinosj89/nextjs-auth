e6fc866793edbdc1ab29c4730b052e5c5b4aa3b0
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2024-01-24 19:50:59 -0800

    testing more home page stuff

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-
+import Navbar from '../components/Navbar';
 import React from 'react'
 
 export default function HomePage() {
@@ -7,7 +7,9 @@ export default function HomePage() {
 
     return (
         <div className="p-12 flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-100 to-indigo-500">
+            <Navbar />
             <div className="p-8 flex flex-col bg-white text-gray-500 text-2xl border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 shadow-2xl">
+
                 <h1>Home</h1>
             </div>
         </div>

2e2b94df404f019411303305f6e38dc24e31bc04
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2024-02-01 18:51:06 -0800

    adding username instead of profile id

--- src/app/profile/[id]/page.jsx
+++ src/app/profile/[id]/page.jsx
@@ -1,8 +1,24 @@
 "use client"
 import axios from 'axios'
 import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
 export default function UserProfile({ params }) {
     const router = useRouter();
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('/api/users/me');
+                setUserData(response.data.data);
+            } catch (error) {
+                console.log(error.message);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     const logout = async () => {
         try {
             // added /api instead of just api and worked due to the file structure
@@ -17,14 +33,19 @@ export default function UserProfile({ params }) {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
-            <hr />
-            <p className="text-4xl">Profile Page
-                <span className="p-2 ml-2 rounded bg-teal-500 text-white">{params.id}</span></p>
-
-            <button className="p-2 mb-1 border rounded-lg text-white bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-300 ..."
-                onClick={logout}>
-                Logout </button>
+            {userData ? (
+                <>
+                    <h1 className="text-4xl">Profile</h1>
+                    <hr />
+                    <p>Hello, {userData.username}</p>
+                    <button className="p-2 mb-1 border rounded-lg text-white bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-300 ..."
+                        onClick={logout}>
+                        Logout
+                    </button>
+                </>
+            ) : (
+                <p>Loading user data...</p>
+            )}
         </div>
     )
 }

0e91cee8ff663c196084055f5513ae7e2994c88f
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2024-02-21 16:25:11 -0800

    adding a todos list

---
+++ src/app/todos/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+import { useState } from 'react'
+export default function Todos() {
+    const [inputText, setInputText] = useState("")
+
+    return (
+        <div className="">
+            <div className="text-2xl">Todo List Nextjs</div>
+            <div className="">
+                <input type="text" placeholder="Enter Todo" value={inputText} onChange={(e) => setInputText(e.target.value)}></input>
+            </div>
+        </div>
+    )
+}
